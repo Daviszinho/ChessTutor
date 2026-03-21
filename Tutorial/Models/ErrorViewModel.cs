333f3223247b341db6be66d968f401c5adf692b2
Daviszinho <dpenaranda@gmail.com>
2026-03-20 22:25:37 -0600

    Eliminar todo el código .NET - proyecto vanilla con HTML/JS puro

--- Tutorial/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ChessTutorWeb.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
